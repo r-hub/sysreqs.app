fadb0dc02abdd456a69afa3bdf7fb58c112ba24d
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-16 18:04:35 +0000

    Refactoring, populate error handling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var populate = require('./routes/populate');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/populate', populate);
+app.use('/', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var populate = require('../lib/populate');
 
-router.get("/", function(req, res) {
+router.get("/populate", function(req, res) {
 
     populate();
     
