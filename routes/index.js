11f09018b42c60f49df9b46e5cbd2a93de264533
Gábor Csárdi <csardi.gabor@gmail.com>
2019-01-20 13:45:48 -0600

    Update dependencies
    Also add a lock file.

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var markedMan = require('marked-man');
+var marked = require('marked');
 
-// ------------------------------------------------------------------
-// A simple summary of the API
-
-router.get("/", function(req, res) {
+router.get('/', function(req, res) {
     fs.readFile('./docs/api.md', function(err, data) {
 	if (err) { throw(err); return; }
 
-	markedMan(
-	    data.toString(),
-	    { format: 'html',
-	      gfm: true
-	    },
-	    function(err, html) {
-		if (err) { throw(err); return; }
-		res.render('api', {
-		    'html': html,
-		    'partials': { 'ghcorner': 'ghcorner' }
-		})
-	    }
-	)
+	marked(data.toString(), { 'format': 'html' }, function(err, html) {
+	    if (err) { console.log(err); throw(err); return; }
+	    res.render('api', {
+		'html': html,
+		'partials': { 'ghcorner': 'ghcorner' }
+	    })
+	})
     })
 })
 
